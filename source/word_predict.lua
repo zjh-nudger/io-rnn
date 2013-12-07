0e607487b646f6e237225c43e754b2d6fbff8435
phong <lephong.xyz@gmail.com>
2013-12-07 01:07:43 +0100

    avg rank on wsj22 is around 47 (with 5000 random)

--- source/word_predict.lua
+++ source/word_predict.lua
@@ -13,6 +13,33 @@ if #arg == 3 then
 	print('loading net...')
 	local net = IORNN:load(net_path)
 
+--[[
+	local dim = net.dim
+	local mul = 0.1	
+	-- unary branch inner
+	net.Wui = uniform(dim, dim, -1, 1):mul(mul)
+	net.bui = uniform(dim, 1, -1, 1):mul(0)
+
+	-- binary branch inner
+	net.Wbil = uniform(dim, dim, -1, 1):mul(mul)	--left
+	net.Wbir = uniform(dim, dim, -1, 1):mul(mul)	--right
+	net.bbi = uniform(dim, 1, -1, 1):mul(0)
+
+	-- binary brach outer
+	net.Wbol = uniform(dim, dim, -1, 1):mul(mul)	--left sister
+	net.Wbor = uniform(dim, dim, -1, 1):mul(mul)	--right sister
+	net.Wbop = uniform(dim, dim, -1, 1):mul(mul)	--parent
+	net.bbol = uniform(dim, 1, -1, 1):mul(0)
+	net.bbor = uniform(dim, 1, -1, 1):mul(0)
+
+	-- word ranking
+	net.Wwi = uniform(2*dim, dim, -1, 1):mul(mul)	-- for combining inner, outer meanings
+	net.Wwo = uniform(2*dim, dim, -1, 1):mul(mul)
+	net.bw = uniform(2*dim, 1, -1, 1):mul(0)
+	net.Ws = uniform(1, 2*dim, -1, 1):mul(mul)	-- for scoring
+
+	net.L = torch.randn(net.L:size()):mul(0.0001)
+]]
 	-- load dic
 	local f = torch.DiskFile(dic_path)
 	local dic = f:readObject()
@@ -27,6 +54,8 @@ if #arg == 3 then
 		n_words = tree.cover[2] - tree.cover[1] + 1
 		tree = tree:to_torch_matrices(dic, 1)
 		treebank[#treebank+1] = tree
+
+		--if #treebank > 2 then break end
 	end
 
 	-- parse treebank
@@ -36,7 +65,7 @@ if #arg == 3 then
 	-- predict words
 	print('evaluating...')
 	local total = 0
-	local correct = 0
+	local rank = 0
 	local func = net.func
 	local Wwo = net.Wwo
 	local Wwi = net.Wwi
@@ -46,29 +75,45 @@ if #arg == 3 then
 	local vocabsize = L:size(2)
 
 	local WwiL = Wwi * L
-	local big_bw = torch.repeatTensor(bw, 1, vocabsize)
+	local n = 5000
+	local big_bw = torch.repeatTensor(bw, 1, n)
 
-	torch.setnumthreads(5)
+	--torch.setnumthreads(5)
 	for j,tree in ipairs(treebank) do
 		print(j)
 		for i = 1,tree.n_nodes do
-			local outer = tree.outer[{{},{i}}]
-			local word_io = func(
-						torch.repeatTensor(Wwo*outer, 1, vocabsize)
-						:add(WwiL):add(big_bw))
-			local word_score = Ws * word_io
-			_,pw_id = torch.max(word_score,2)
+			if tree.n_children[i] == 0 then
+				local outer = tree.outer[{{},{i}}]
+				local word_id = tree.word_id[i]
+
+				local index = torch.rand(n-1):mul(vocabsize):floor():add(1):long()
+				local small_WwiL = torch.Tensor(WwiL:size(1), n)
+				small_WwiL[{{},{1,n-1}}]:copy(WwiL:index(2,index))
+				small_WwiL[{{},{n}}]:copy(WwiL[{{},{word_id}}])
+
+				local word_io = func(
+						torch.repeatTensor(Wwo*outer, 1, n)
+						:add(small_WwiL):add(big_bw))
+				local word_score = (Ws * word_io):reshape(n)
+				_,id = word_score:sort(true)
 			
-			total = total + 1
-			if pw_id[{1,1}] == tree.word_id[i] then 
-				correct = correct + 1
+				_,id = id:max(1)
+				rank = rank + id[1]
+				total = total + 1
+				print(rank / total)
+
+				--[[print('------------')
+				print(word_score)
+				print(word_score:sort(true))
+				print(id[1])
+				print(dic.id2word[word_id])
+				print(word_id) ]]
 			end
-			print(correct / total)
 		end
 		collectgarbage()
 	end
 
-	print(correct/ total)
+	print(rank/ total)
 	
 else 
 	print("[iornn] [treebank] [dic]")
